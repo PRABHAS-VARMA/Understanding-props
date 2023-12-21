dd76b1ef2dcd584b4c47568f452464c94b54e404
PRABHAS VARMA <prabhasvarma017@gmail.com>
2023-12-21 15:47:53 +0530

    updated

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// App.jsx
+
+import React from "react";
+import "./App.css";
+import DataComponent from "./components/DataComponent";
+
+function App({ props }) {
+
+  return (
+    <div>
+      <img src={props.img} alt=""  />
+    </div>
+  );
+}
+
+export default App;
